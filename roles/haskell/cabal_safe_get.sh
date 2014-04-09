a9802b8c2eeaaf2722f343762446568ce8bd3e23
philopon <philopon.dependence@gmail.com>
2014-04-10 01:56:44 +0900

    add haskell tools

--- roles/haskell/cabal_safe_get.sh
+++ roles/haskell/cabal_safe_get.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-get=`cabal get $1 -d $2`
+get=`cabal get $1 -d $2 2>&1`
 
 if [ $? -eq 0 ]; then
-  echo $get | awk 'NR == 1{ print $3 }'
+  echo $get | awk 'NR == 1 { print $3 }'
 else
   echo $get | awk 'BEGIN{ FS = "\""} NR == 1 { print $2 }'
 fi
